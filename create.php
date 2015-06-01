4a04990473ca9a3f63b67c10fbdcb407c3c4c71f
kriss0r <kriss@ceusmedia.de>
2015-06-01 21:25:05 +0200

    Use classmap autoloader and update create script.

--- create.php
+++ create.php
@@ -9,15 +9,33 @@
  *	Therefore the first 2 lines give to possible presettings.
  *	Please select one and remove the other.
  */
+
+$pathSelf			= __DIR__ . '/';
+$pathCmCommon		= "";
+$pathMarkdown		= "";
+
+
+if (file_exists('vendor/autoload.php')) {
+	require_once 'vendor/autoload.php';
+} else {
+	if (!($pathCmCommon && file_exists($pathCmCommon . '/autoload.php'))) {
+		die('Please install CeusMedia/Common first and set path in $pathCmCommon!' . PHP_EOL);
+	}
+	require_once $pathCmCommon . '/autoload.php';
+	if ($pathMarkdown && file_exists($pathMarkdown)) {
+		CMC_Loader::registerNew('php', 'Michelf\\', $pathMarkdown . 'Michelf/');
+	}
+	CMC_Loader::registerNew('php5', 'DocCreator_', $pathSelf . '/classes/');			//  enable class auto loading for DocCreator
+}
+
+/*
 $pathLib			= __DIR__."/lib/";
 $pathCmClasses		= file_exists( $pathLib ) ? $pathLib."cmClasses/" : "cmClasses/trunk/";
 $pathPhpMarkdown	= file_exists( $pathLib ) ? $pathLib."php-markdown/" : "/var/www/lib/php-markdown/";
 
 @require_once $pathCmClasses.'autoload.php5';
 if( !class_exists( 'CMC_Loader' ) ) die( "Please install cmClasses first!");
-
-CMC_Loader::registerNew( 'php', 'Michelf\\', $pathPhpMarkdown.'Michelf/' );
-CMC_Loader::registerNew( 'php5', 'DocCreator_', __DIR__.'/classes/' );	//  enable class auto loading for DocCreator
+*/
 
 new DocCreator_Core_ConsoleRunner();									//  open new starter
 ?>
